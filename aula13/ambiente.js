e9029414c2a62d1f99a6ff48bff4d31afbceb907
arthurguerraa <127884471+arthurguerraa@users.noreply.github.com>
2025-04-25 15:38:26 -0300

    whiile, do while e for

---
+++ aula13/ambiente.js
@@ -0,0 +1,13 @@
+// WHILE - TESTE LÓGICO NO COMEÇO
+var i = 1
+while(i <= 6){
+    console.log(`Passo ${i}`)
+    i++
+}
+
+// DO WHILE - TESTE LÓGICO NO FINAL
+do{
+    console.log(`Passo ${i}`)
+    i++
+}while(i <= 6)
+
---
+++ aula14/ambiente.js
@@ -0,0 +1,6 @@
+// FOR
+console.log('Vai começar...')
+for(var i = 1; i <= 6; i++){
+    console.log(i)
+}
+console.log('FIM!')
