9585515d66f11ea3e7b322ea7f74753364871cf9
arthurguerraa <127884471+arthurguerraa@users.noreply.github.com>
2025-05-02 18:17:14 -0300

    script

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -3,8 +3,8 @@ function carregar(){
     var img = document.getElementById('imagem')
 
     var data = new Date()
-    //var hora = data.getHours()
-    var hora = 10
+    var hora = data.getHours()
+
     msg.innerHTML = 'Agora são ' + hora + ' horas.'
     if(hora >= 0 && hora < 12){
         // BOM DIA!
