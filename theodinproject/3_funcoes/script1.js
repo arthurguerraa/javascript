d869073085dbc99ca4e5b30a22df149b061f5971
arthurguerraa <127884471+arthurguerraa@users.noreply.github.com>
2025-08-09 18:09:35 -0300

    exercicio funcoes

---
+++ theodinproject/3_funcoes/script1.js
@@ -0,0 +1,17 @@
+// 1- Crie uma função chamada add7
+
+function add7(a){
+    return a + 7;
+}
+
+console.log(add7(1));
+
+// 2- Crie uma função chamada multiply
+
+function multiply(a, b){
+    return a * b;
+}
+
+console.log(multiply(2, 2));
+
+// 3- Crie uma função chamada capitalize
