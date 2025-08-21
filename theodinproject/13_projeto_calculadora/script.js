99e7357515bc44dc91a5eb0547090edccfefd359
arthurguerraa <127884471+arthurguerraa@users.noreply.github.com>
2025-08-21 18:42:38 -0300

    função calcular e variaveis para o calculo

--- theodinproject/13_projeto_calculadora/script.js
+++ theodinproject/13_projeto_calculadora/script.js
@@ -1,3 +1,7 @@
+const n1 = undefined;
+const operador = undefined;
+const n2 = undefined;
+
 function soma(a, b){
     return a + b;
 }
@@ -12,4 +16,24 @@ function multiplicar(a, b){
 
 function dividir(a, b){
     return a / b;
+}
+
+function calcular(n1, operador, n2){
+    switch(operador){
+        case "+":
+            soma(n1, n2);
+            break;
+        case "-":
+            subtrair(n1, n2);
+            break;
+        case "*":
+            multiplicar(n1, n2);
+            break;
+        case "/":
+            dividir(n1, n2);
+            break;
+        default:
+            console.log("Operador inválido")        
+
+    }
 }
