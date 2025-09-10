69ea9829a3ae6e678321726102a22fce4df91f92
arthurguerraa <127884471+arthurguerraa@users.noreply.github.com>
2025-09-10 19:15:28 -0300

    estruturando o js e o html

--- theodinproject/15_projeto_livraria/script.js
+++ theodinproject/15_projeto_livraria/script.js
@@ -1,29 +1,47 @@
-const biblioteca = [];
+let biblioteca = [];
 
 function Livro(titulo, autor, paginas, genero, lido){
     if(!new.target){
         throw Error("Você deve usar o operador 'new' para chamar o construtor")
     }
-    this.id = crypto.randomUUID;
+    this.id = crypto.randomUUID();
     this.titulo = titulo;
     this.autor = autor;
     this.paginas = paginas;
     this.genero = genero;
     this.lido = lido;
+}
 
-    this.info = function(){
-        if(this.read === false){
-            return `${this.title} de ${this.author}, ${this.pages} páginas, gênero ${genero} e ainda não lido`;
+    Livro.prototype.info = function(){
+        if(this.lido === false){
+            return `${this.titulo} de ${this.autor}, ${this.paginas} páginas, gênero ${this.genero} e ainda não lido`;
         }else{
-            return `${this.title} de ${this.author}, ${this.pages} páginas, gênero ${genero} e já foi lido`;
+            return `${this.titulo} de ${this.autor}, ${this.paginas} páginas, gênero ${this.genero} e já foi lido`;
         }
     }
 
-    this.adicionaLivro = function(livro){
+    function adicionaLivro(titulo, autor, paginas, genero, lido){
+        const livro = new Livro(titulo, autor, paginas, genero, lido);
         biblioteca.push(livro);
     }
-}
 
-const livro1 = new Livro('A Metamorfose', 'Franz Kafka', 200, 'Ficção', false);
-livro1.adicionaLivro(livro1);
+    function displayBooks(){
+        const container = document.querySelector("#books-container");
+        container.innerHTML = "";
+
+        biblioteca.forEach(livro => {
+            const card = document.createElement("div");
+            card.classList.add("book-card");
+
+            card.innerHTML = `
+                <h3>${livro.titulo}</h3>
+                <p>Autor: ${livro.autor}</p>
+                <p>Páginas: ${livro.paginas}</p>
+                <p>Status: ${livro.lido ? "lido" : "não lido"}
+            `
+            container.appendChild(card);
+        });
+    }
+
+adicionaLivro("Harry Potter", "J.K. Rowling", "200", "Ficção", false);
 console.log(biblioteca);
